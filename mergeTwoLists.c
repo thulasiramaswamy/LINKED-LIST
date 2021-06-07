28a67a2d6875da659174aed1e06839bb6eee8427
Thulasi Veggalam <19488277+tveggala@users.noreply.github.com>
2021-06-07 08:45:16 +0530

    Update mergeTwoLists.c

--- mergeTwoLists.c
+++ mergeTwoLists.c
@@ -4,122 +4,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* A Simple and Typical Node Structure */
 struct Node
 {
 	int dat;
-	struct Node *next;
-};
-
-/* Allocates memory dynamically and returns a new node */
-struct Node* new_node(int value)
+	struct Node* next;
+}Node;
+struct Node* new_Node(int value)
 {
 	struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
 	temp->dat = value;
 	temp->next = NULL;
 	return temp;
 }
-
-/* Creates a node, if List is empty
- * Appends node, if List is not empty
- */
-void createAndAppend(struct Node **head_ref, int value)
+/* Creates a Fresh list, if the List is empty
+*  Appends the Node into the list, if the List is not Empty
+*/
+void createAndAppend(struct Node** head_ref, int value)
 {
 	struct Node* temp = *head_ref;
 	if (temp != NULL)
 	{
 		while (temp->next != NULL) temp = temp->next;
-		temp->next = new_node(value);
+		temp->next = new_Node(value);
 		return;
 	}
-	*head_ref = new_node(value);
+	*head_ref = new_Node(value);
 }
-
-/* Merges two sorted lists and returns head node */
-struct Node* sortedMerge(struct Node* a, struct Node* b)
+/*
+* Function that merges two sorted Lists
+*/
+struct Node* merge(struct Node* head1, struct Node* head2)
 {
-	if (a == NULL) return b;
-	if (b == NULL) return a;
-
-	struct Node* head = NULL;
-	struct Node* tail = NULL;
-
-	/* head and tail initailization */
-	if (a->dat <= b->dat)
+	struct Node* newhead;
+	struct Node* sorting = NULL;
+	if (head1 == NULL)		return head2;
+	if (head2 == NULL)		return head1;
+	if (head1 && head2)
 	{
-		head = tail = a;
-		a = a->next;
-	}
-	else
-	{
-		head = tail = b;
-		b = b->next;
-	}
-
-	while (a != NULL && b != NULL)
-	{
-		if (a->dat <= b->dat)
+		if (head1->dat <= head2->dat)
 		{
-			tail->next = a;
-			tail = a;
-			a = a->next;
+			sorting = head1;
+			head1 = sorting->next;
 		}
 		else
 		{
-			tail->next = b;
-			tail = b;
-			b = b->next;
+			sorting = head2;
+			head2 = sorting->next;
 		}
 	}
-
-	/* If any of the list has extra nodes in the end
-	* Just append in the end
-	* Here, If 'a' has nodes left out, attach 'a' to tail
-	* else attach 'b' to tail
-	*/
-	if (a == NULL) tail->next = b;
-	else tail->next = a;
-
-	return head;
+	newhead = sorting;
+	while (head1 && head2)
+	{
+		if (head1->dat<= head2->dat)
+		{
+			sorting->next = head1;
+			sorting = head1;
+			head1 = sorting->next;
+		}
+		else
+		{
+			sorting->next = head2;
+			sorting = head2;
+			head2 = sorting->next;
+		}
+	}
+	if (head1 == NULL)		sorting->next = head2;
+	if (head2 == NULL)		sorting->next = head1;
+	return newhead;
 }
-
-/* Print the list */
-void printList(struct Node *head_ref)
+/*
+* Driver Function
+*/
+int main()
 {
- /* Base Condition to Check a List is Empty or Not */
- if (head_ref == NULL) 
- { 
-  printf("\nEmpty List !!! \n"); 
-  return;
- }
- struct Node* temp = head_ref;
+	struct Node* head1 = NULL;
+	struct Node* head2 = NULL;
+	struct Node* sortedHead = NULL;
+	int i, arr[] = { 1, 3, 5, 7, 9, 11, 12, 14, 15, 16 };
+	int arr1[] = { 2, 4, 6, 8, 10 };
 
- printf("\nElements in the list :\n");
- 
- while (temp != NULL)
- {
-  printf(" %d ", temp->dat);
-  temp = temp->next;
- }
-}
+	for (i = 0; i < sizeof(arr) / sizeof(int); i++)
+		createAndAppend(&head1, *(arr + i));
 
-/* Driver Function */
-int main(void)
-{
- struct Node* head1 = NULL;
- struct Node* head2 = NULL;
- 
- int i;
- int arr1[] = {1, 10, 15, 45 };
- int arr2[] = { 5, 18, 25, 40, 50, 60 };
+	for (i = 0; i < sizeof(arr1) / sizeof(int); i++)
+		createAndAppend(&head2, *(arr1 + i));
 
-for (i = 0; i < sizeof(arr1) / sizeof(arr1[0]) ; i++)
-	createAndAppend(&head1, *(arr1+i));
+	sortedHead = merge(head1, head2);
 
-for (i = 0; i < sizeof(arr2) / sizeof(arr2[0]); i++)
-	createAndAppend(&head2, *(arr2 + i));
-	
- struct Node* res = sortedMerge(head1, head2);
- printList(res);
- return 0;
+	printf("\n Sorted Merged Linked List =>  ");
+	while (sortedHead != NULL)
+	{
+		printf("%d  ", sortedHead->dat);
+		sortedHead = sortedHead->next;
+	}
+	return 0;
 }
